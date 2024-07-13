b275d6d26d01b7a272bb06945f76dd1988742df8
Belhavensky <Schuleeee123@yandex.ru>
2024-07-13 13:35:09 +0500

    Отредактирован первоначальный файл main.py. Добавлены новые символы, удалено обращение к пользователю по фамилии.

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ bot = telebot.TeleBot('Key')
 
 @bot.message_handler()
 def info(message):
-    if message.text.lower() == 'Hello':
-        bot.send_message(message.chat.id, f'Hello, {message.from_user.first_name} {message.from_user.last_name}')
+    if message.text.lower() == 'Hello!':
+        bot.send_message(message.chat.id, f'Hello!, {message.from_user.first_name}')
     elif message.text.lower() == 'id':
         bot.reply_to(message, f'ID:{message.from_user.id}')
 
@@ -16,4 +16,4 @@ while True:
     try:
         bot.polling(none_stop=True)
     except Exception as e:
-        print(f"❌Error. {e}")
+        print(f"❌Error!. {e}")
