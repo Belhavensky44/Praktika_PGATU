980edc4c41e1e2d976bfb9db47a5257fbe4a80f7
Belhavensky <Schuleeee123@yandex.ru>
2024-07-13 12:52:16 +0500

    Добавлен файл main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import telebot
+
+
+bot = telebot.TeleBot('Key')
+
+@bot.message_handler()
+def info(message):
+    if message.text.lower() == 'Hello':
+        bot.send_message(message.chat.id, f'Hello, {message.from_user.first_name} {message.from_user.last_name}')
+    elif message.text.lower() == 'id':
+        bot.reply_to(message, f'ID:{message.from_user.id}')
+
+while True:
+    try:
+        bot.polling(none_stop=True)
+    except Exception as e:
+        print(f"❌Error. {e}")
