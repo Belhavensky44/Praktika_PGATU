c17ba218e1391ebdb1c3dbdfa1fb11bd29c82fc4
Belhavensky <Schuleeee123@yandex.ru>
2024-07-13 13:19:44 +0500

    Ответы программы руссифицированы

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import webbrowser
 from telebot import types
 
 
-bot = telebot.TeleBot('Key')
+bot = telebot.TeleBot('Ключ')
 
 @bot.message_handler()
 def info(message):
-    if message.text.lower() == 'Hello':
-        bot.send_message(message.chat.id, f'Hello, {message.from_user.first_name} {message.from_user.last_name}')
+    if message.text.lower() == 'Привет':
+        bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name} {message.from_user.last_name}')
     elif message.text.lower() == 'id':
         bot.reply_to(message, f'ID:{message.from_user.id}')
 
@@ -16,4 +16,4 @@ while True:
     try:
         bot.polling(none_stop=True)
     except Exception as e:
-        print(f"❌Error. {e}")
+        print(f"❌Ошибка. {e}")
